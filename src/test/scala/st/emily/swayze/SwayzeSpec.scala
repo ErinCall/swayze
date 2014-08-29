1852e4970f88883037778fc8f64804b5de7e877d
Emily St <hello@emily.st>
2014-08-28 18:56:42 -0700

    Stub out the first test

---
+++ src/test/scala/st/emily/swayze/SwayzeSpec.scala
@@ -0,0 +1,16 @@
+package st.emily.swayze.tests
+
+import com.simple.simplespec.Spec
+import org.junit.Test
+
+import st.emily.swayze.SwayzeApp
+
+
+class SwayzeSpec extends Spec {
+  class SwayzeApp {
+    @Test def `stub test` = {
+      1.must(be(1))
+    }
+  }
+}
+
