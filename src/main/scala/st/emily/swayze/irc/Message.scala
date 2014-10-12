664a770be2a8ad88aaaa76b6cf6ab31c23915341
Emily St <hello@emily.st>
2014-10-11 22:27:13 -0700

    Gross inherited constructor shit

--- src/main/scala/st/emily/swayze/irc/Message.scala
+++ src/main/scala/st/emily/swayze/irc/Message.scala
@@ -1,20 +1,20 @@
 package st.emily.swayze.irc
 
 
-abstract sealed class Message(prefix:     Option[String],
-                              command:    String,
-                              parameters: Seq[String]) // up to 15 params
+abstract sealed class Message(val prefix:     Option[String],
+                              val command:    String,
+                              val parameters: Seq[String]) // up to 15 params
 
-case class Privmsg(prefix:     Option[String],
-                   command:    String,
-                   parameters: Seq[String],
-                   action:     Boolean,
-                   target:     String) extends Message(prefix, command, parameters)
+case class Privmsg(override val prefix:     Option[String],
+                   override val command:    String,
+                   override val parameters: Seq[String],
+                   action:                  Boolean,
+                   target:                  String) extends Message(prefix, command, parameters)
 
-case class Numerical(prefix:     Option[String],
-                     command:    String,
-                     parameters: Seq[String],
-                     number:     Short) extends Message(prefix, command, parameters)
+case class Numerical(override val prefix:     Option[String],
+                     override val command:    String,
+                     override val parameters: Seq[String],
+                     number:                  Short) extends Message(prefix, command, parameters)
 
 
 object Message extends MessageParser {
