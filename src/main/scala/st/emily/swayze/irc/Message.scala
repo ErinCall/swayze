b9d4cf88555f9354d21445a52181d4db0439c114
Emily St <hello@emily.st>
2014-10-11 22:21:07 -0700

    I hate the way this is going

--- src/main/scala/st/emily/swayze/irc/Message.scala
+++ src/main/scala/st/emily/swayze/irc/Message.scala
@@ -1,12 +1,35 @@
 package st.emily.swayze.irc
 
 
-abstract sealed class Message
+abstract sealed class Message(prefix:     Option[String],
+                              command:    String,
+                              parameters: Seq[String]) // up to 15 params
 
-case class Privmsg() extends Message
+case class Privmsg(prefix:     Option[String],
+                   command:    String,
+                   parameters: Seq[String],
+                   action:     Boolean,
+                   target:     String) extends Message(prefix, command, parameters)
 
-object Message {
+case class Numerical(prefix:     Option[String],
+                     command:    String,
+                     parameters: Seq[String],
+                     number:     Short) extends Message(prefix, command, parameters)
+
+
+object Message extends MessageParser {
   def apply(line: String): Message = {
-    Privmsg()
+    val (prefix, command, parameters) = parse(line)
+
+    command.toUpperCase match {
+      case "PRIVMSG" => Privmsg(prefix, command, parameters, false, "")
+    }
+  }
+}
+
+
+trait MessageParser {
+  def parse(text: String): (Option[String], String, Seq[String]) = {
+    (Option(""), "", Seq())
   }
 }
