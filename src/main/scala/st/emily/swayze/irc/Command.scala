70a47c5a7ccab29c0c092d1f8aabc21e3e01c8ff
Emily St <hello@emily.st>
2014-10-11 23:21:34 -0700

    Move enums into their own files

---
+++ src/main/scala/st/emily/swayze/irc/Command.scala
@@ -0,0 +1,50 @@
+package st.emily.swayze.irc
+
+
+object Command extends Enumeration {
+  type Command = Value
+
+  val AWAY     = Value
+  val CONNECT  = Value
+  val DIE      = Value
+  val INFO     = Value
+  val INVITE   = Value
+  val ISON     = Value
+  val JOIN     = Value
+  val KICK     = Value
+  val KILL     = Value
+  val LINKS    = Value
+  val LIST     = Value
+  val MOTD     = Value
+  val NAMES    = Value
+  val NOTICE   = Value
+  val NUMERIC  = Value  // internal command
+  val OPER     = Value
+  val PART     = Value
+  val PASS     = Value
+  val PING     = Value
+  val PONG     = Value
+  val PRIVMSG  = Value
+  val QUIT     = Value
+  val REHASH   = Value
+  val RESTART  = Value
+  val SERVICE  = Value
+  val SERVLIST = Value
+  val SQUERY   = Value
+  val SQUIT    = Value
+  val STATS    = Value
+  val SUMMON   = Value
+  val TIME     = Value
+  val TOPIC    = Value
+  val TRACE    = Value
+  val UNKNOWN  = Value  // internal command
+  val USER     = Value
+  val USERHOST = Value
+  val USERS    = Value
+  val VERSION  = Value
+  val WALLOPS  = Value
+  val WHO      = Value
+  val WHOIS    = Value
+  val WHOWAS   = Value
+}
+
