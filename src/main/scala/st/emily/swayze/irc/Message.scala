564d72ea07e3cbc33baa02bd02e51d73acd06f00
Emily St <hello@emily.st>
2014-10-18 19:29:43 -0700

    Just use a single Message class for now
    I admit defeat on the factory pattern for now. Just a single type to
    encapsulate all IRC events.

--- src/main/scala/st/emily/swayze/irc/Message.scala
+++ src/main/scala/st/emily/swayze/irc/Message.scala
@@ -3,9 +3,36 @@ package st.emily.swayze.irc
 import scala.util.{ Failure, Success, Try }
 
 import Command.Command
+import Numeric.Numeric
 
 
-trait MessageParser {
+/**
+ * This type represents the contents of a single IRC event.
+ *
+ * I'd like to break this into many types, one for each kind of
+ * message, but it's going to be a pain, and I'd like to move on for
+ * now.
+ */
+case class Message(raw:        Option[String],
+                   prefix:     Option[String],
+                   command:    Option[Command],
+                   numeric:    Option[Numeric],
+                   parameters: Seq[String]) {
+  def toRaw = raw.getOrElse {
+    (Seq(prefix.getOrElse(""), command.getOrElse(numeric.get.id)) ++ parameters).mkString("\u0020") + "\r\n"
+  }
+
+  def action:   Boolean = false
+  def target:   String  = parameters(0)
+  def contents: String  = parameters(1)
+}
+
+object Message {
+  def apply(line: String): Message = {
+    val (prefix, command, numeric, parameters) = parse(line)
+    new Message(Option(line), prefix, command, numeric, parameters)
+  }
+
   /**
    * Breaks a raw IRC message string into parts ready for a Message
    * constructor.
@@ -19,66 +46,21 @@ trait MessageParser {
    *
    * @see http://tools.ietf.org/html/rfc2812#section-2.3.1
    */
-  def parse(text: String): (Option[String], Command, Option[String], Seq[String]) = {
-    val tokens = text.split("\u0020").map(_.trim)
-
-    val prefix = tokens(0)(0) match {
-      case ':' => Option(tokens(0))
-      case _ => None
-    }
-
+  def parse(text: String): (Option[String], Option[Command], Option[Numeric], Seq[String]) = {
+    val tokens         = text.split("\u0020").map(_.trim)
+    val prefix         = tokens(0)(0) match {
+                           case ':' => Option(tokens(0))
+                           case _   => None
+                         }
     val commandOrReply = tokens(if (prefix.isDefined) 1 else 0).toUpperCase
-    val command =
-      Try(Command.withName(commandOrReply)) match {
-        case Success(command) => command
-        case Failure(_)       =>
-          Try(commandOrReply.toInt) match {
-            case Success(_) => Command.REPLY
-            case Failure(_) => Command.UNKNOWN
-          }
-      }
-
-    val numeric = if (command == Command.REPLY) Option(commandOrReply) else None
-    val params = tokens.drop(if (prefix.isDefined) 2 else 1).takeWhile(!_.startsWith(":"))
-    val trailing = text.split(":", 3).lastOption.map(_.stripLineEnd)
+    val command        = Try(Command.withName(commandOrReply)) match {
+                           case Success(command) => Option(command)
+                           case Failure(_)       => None
+                         }
+    val numeric        = if (!command.isDefined) Option(Numeric.withName(commandOrReply)) else None
+    val params         = tokens.drop(if (prefix.isDefined) 2 else 1).takeWhile(!_.startsWith(":"))
+    val trailing       = text.split(":", 3).lastOption.map(_.stripLineEnd)
 
     (prefix, command, numeric, params ++ trailing)
   }
 }
-
-
-object Message extends MessageParser {
-  def apply(line: String): Message = {
-    val (prefix, command, numeric, parameters) = parse(line)
-
-    command match {
-      case Command.PRIVMSG => Privmsg(Option(line), prefix, command, parameters, false, parameters(0))
-      case Command.REPLY   => Reply(Option(line), prefix, command, parameters, numeric.get)
-    }
-  }
-}
-
-
-abstract sealed class Message(val raw:        Option[String] = None,
-                              val prefix:     Option[String] = None,
-                              val command:    Command,
-                              val parameters: Seq[String]    = Seq()) {
-  def toRaw = raw.getOrElse(
-    (Seq(prefix.getOrElse(""), command) ++ parameters).mkString(" ") + "\r\n"
-  )
-}
-
-
-case class Privmsg(override val raw:        Option[String] = None,
-                   override val prefix:     Option[String] = None,
-                   override val command:    Command,
-                   override val parameters: Seq[String]    = Seq(),
-                   action:                  Boolean,
-                   target:                  String) extends Message(raw, prefix, command, parameters)
-
-
-case class Reply(override val raw:        Option[String] = None,
-                 override val prefix:     Option[String] = None,
-                 override val command:    Command,
-                 override val parameters: Seq[String]    = Seq(),
-                 numeric:                 String) extends Message(raw, prefix, command, parameters)
--- src/test/scala/st/emily/swayze/MessageSpec.scala
+++ src/test/scala/st/emily/swayze/MessageSpec.scala
@@ -11,41 +11,53 @@ class MessageSpec extends Spec {
   class `MessageParser` {
     @Test def `Parses PRIVMSG` = {
       val message = Message(":nick!ident@host.name PRIVMSG target :This is a message\r\n")
-      message.must(be(Privmsg(raw        = Option(":nick!ident@host.name PRIVMSG target :This is a message\r\n"),
+
+      message.must(be(Message(raw        = Option(":nick!ident@host.name PRIVMSG target :This is a message\r\n"),
                               prefix     = Option(":nick!ident@host.name"),
-                              command    = Command.PRIVMSG,
+                              command    = Option(Command.PRIVMSG),
                               parameters = Seq("target", "This is a message"),
-                              action     = false,
-                              target     = "target")))
+                              numeric    = None)))
+
+      message.action.must(be(false))
+      message.target.must(be("target"))
+      message.contents.must(be("This is a message"))
     }
 
     @Test def `Parses PRIVMSG with a colon in the message` = {
       val message = Message(":nick!ident@host.name PRIVMSG target :This is a : message\r\n")
-      message.must(be(Privmsg(raw        = Option(":nick!ident@host.name PRIVMSG target :This is a : message\r\n"),
+
+      message.must(be(Message(raw        = Option(":nick!ident@host.name PRIVMSG target :This is a : message\r\n"),
                               prefix     = Option(":nick!ident@host.name"),
-                              command    = Command.PRIVMSG,
+                              command    = Option(Command.PRIVMSG),
                               parameters = Seq("target", "This is a : message"),
-                              action     = false,
-                              target     = "target")))
+                              numeric    = None)))
+
+      message.action.must(be(false))
+      message.target.must(be("target"))
+      message.contents.must(be("This is a : message"))
     }
 
     @Test def `Parses PRIVMSG keeping whitespace` = {
-      val message = Message(":nick!ident@host.name PRIVMSG target : This is a message \r\n")
-      message.must(be(Privmsg(raw        = Option(":nick!ident@host.name PRIVMSG target : This is a message \r\n"),
+      val message = Message(":nick!ident@host.name PRIVMSG target :\t This is a message \r\n")
+
+      message.must(be(Message(raw        = Option(":nick!ident@host.name PRIVMSG target :\t This is a message \r\n"),
                               prefix     = Option(":nick!ident@host.name"),
-                              command    = Command.PRIVMSG,
-                              parameters = Seq("target", " This is a message "),
-                              action     = false,
-                              target     = "target")))
+                              command    = Option(Command.PRIVMSG),
+                              parameters = Seq("target", "\t This is a message "),
+                              numeric    = None)))
+      message.action.must(be(false))
+      message.target.must(be("target"))
+      message.contents.must(be("\t This is a message "))
     }
 
     @Test def `Parses REPLY` = {
       val message = Message(":irc.host 352 someone #channel user 0.0.0.0 irc.host someone G :0 Real Name\r\n")
-      message.must(be(Reply(raw        = Option(":irc.host 352 someone #channel user 0.0.0.0 irc.host someone G :0 Real Name\r\n"),
-                            prefix     = Option(":irc.host"),
-                            command    = Command.REPLY,
-                            parameters = Seq("someone", "#channel", "user", "0.0.0.0", "irc.host", "someone", "G", "0 Real Name"),
-                            numeric    = "352")))
+
+      message.must(be(Message(raw        = Option(":irc.host 352 someone #channel user 0.0.0.0 irc.host someone G :0 Real Name\r\n"),
+                              prefix     = Option(":irc.host"),
+                              command    = None,
+                              parameters = Seq("someone", "#channel", "user", "0.0.0.0", "irc.host", "someone", "G", "0 Real Name"),
+                              numeric    = Option(Numeric.withName("352")))))
     }
   }
 }
