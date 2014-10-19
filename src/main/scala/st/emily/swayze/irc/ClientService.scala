b12953f814149b04f098665ab1efdf6173b060de
Emily St <hello@emily.st>
2014-10-19 00:24:42 -0700

    Changed my mind, simplify, simplify

--- src/main/scala/st/emily/swayze/irc/ClientService.scala
+++ src/main/scala/st/emily/swayze/irc/ClientService.scala
@@ -27,32 +27,16 @@ class ClientService(config: NetworkConfiguration) extends Actor with ActorLoggin
   override val supervisorStrategy = SupervisorStrategy.stoppingStrategy
   override def postRestart(thr: Throwable): Unit = context.stop(self)
 
-  private[this] var messagesReceived: Long = 0
-  private[this] var messagesSent:     Long = 0
-
-  override def postStop: Unit = {
-    log.info(s"Sent $messagesSent messages")
-    log.info(s"Received $messagesReceived messages")
-  }
-
   override def receive: Receive = {
     case Ready =>
       sender() ! Message(Command.NICK, config.nickname)
       sender() ! Message(Command.USER, config.nickname, config.nickname, "*", config.nickname)
 
     case message: Message if message.command == Option(Command.PING) =>
-      messagesReceived += 1
-      sender() ! Message(command    = Option(Command.PONG),
-                         parameters = Seq(message.pingValue.getOrElse("")))
-      messagesSent += 1
+      sender() ! Message(Command.PONG, message.pingValue.getOrElse(""))
 
-    case message: Message if message.numeric.isDefined &&
-                             message.numeric == Option(Numeric.RPL_WELCOME) =>
-      messagesReceived += 1
-      config.channels.foreach { channel => 
-        sender() ! Message(Command.JOIN, channel)
-        messagesSent += 1
-      }
+    case message: Message if message.numeric == Option(Numeric.RPL_WELCOME) =>
+      config.channels.foreach(sender() ! Message(Command.JOIN, _))
 
     case _ =>
   }
